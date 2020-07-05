0503a61c37b2978949bd7b4f436c455867a822a2
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-07-05 13:45:51 -0600

    added engineer, manager, employee, intern classes

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
-const inquirer = require('inquirer');
 
-const questions = [{
-        type: 'input',
-        name: 'managerName',
-        message: 'Enter team managers name (required)',
-        validate: managerInput => {
-            if (managerInput) {
-                return true;
-            } else {
-                console.log('Please enter the team managers name');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: 'Enter managers employee ID (required)',
-        validate: managerIdInput => {
-            if(managerIdInput) {
-                return true;
-            } else {
-                console.log('Please enter your Employee ID');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'Enter managers email address (required)',
-        validate: managerEmailInput => {
-            if(managerEmailInput) {
-                return true;
-            } else {
-                console.log("Please enter the managers email address");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerPhone',
-        message: 'Please enter managers office phone number (required)',
-        validate: managerPhoneInput => {
-            if(managerPhoneInput) {
-                return true;
-            } else {
-                console.log("Please enter managers office phone number");
-                return false;
-            }
-        }
-    }
-]
+
+// function to initialize program
+function init() {
+    this.employees = []
+
+    console.log(`
+    ===========================
+    Hi! Lets generate a team profile!
+    ===========================
+    `);
+ 
+    
+};
+
+// function call to initialize program
+init();
